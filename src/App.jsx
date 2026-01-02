f75bf107e7c046570230cf8850844d1d241494f1
23JZ0109 <23jz0109@jec.ac.jp>
2026-01-02 14:26:33 +0900

    フロントの書き直し

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>T08結合テスト</h1>
+      <p>フロントの書き直し</p>
+    </div>
+  );
+}
+
+export default App;
